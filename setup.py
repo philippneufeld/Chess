d5840afaf13e197780c4d51a2878036a7bde4596
Philipp Neufeld <philipp.neufeld@t-online.de>
2021-09-23 12:19:39 +0200

    Second commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Copyright Philipp Neufeld, 2021
+
+from setuptools import setup
+
+required_packages = [
+    'pygame',
+]
+
+setup(
+    name="Chess",
+    version=1.0,
+    description="Basic chess app",
+    author="Philipp Neufeld",
+    install_requires=required_packages,
+)
